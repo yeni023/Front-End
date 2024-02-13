7a2c95a43b6ff5c6188a8846e0a2f4c1a0563c49
Y2IN <dpdls0228@daum.net>
2024-02-13 16:06:54 +0900

    진행 중

---
+++ apps/genius/src/genre/genre.ts
@@ -0,0 +1,15 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  background-color: green;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const Header = styled.header`
+  display: flex;
+  color: #fff; /* 텍스트 색상 */
+  padding: 1rem; /* 여백 */
+  text-align: center; /* 가운데 정렬 */
+`;
---
+++ apps/genius/src/genre/genre.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./genre";
+
+function Genre() {
+  return (
+    <Container>
+      <header>Genre</header>
+    </Container>
+  );
+}
+
+export default Genre;
