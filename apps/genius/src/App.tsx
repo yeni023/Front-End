198c5d97ebf8e0818cc1acb7444128accc69d47b
HYEJIN KIM <94445693+hyejin5914@users.noreply.github.com>
2024-04-01 02:28:32 +0900

    Revert "혜진 기본정보 업데이트 0401"

--- apps/genius/src/App.tsx
+++ apps/genius/src/App.tsx
@@ -7,8 +7,7 @@ import MainHome from "./pages/MainHome/MainHome.tsx";
 import CreateStory from "./pages/CreateStory/CreateStory.tsx";
 import SelectChar from "./pages/SelectChar/SelectChar.tsx";
 import ThemePageNext from "./pages/Theme/ThemePageNext.tsx";
-import BasicInfoAC from "./pages/BasicInfo/BasicInfoAC.tsx";
-import BasicInfoDC from "./pages/BasicInfo/BasicInfoDC.tsx";
+import BasicInfoPage from "./pages/BasicInfo/BasicInfoPage.tsx";
 import SelectLevel from "./pages/SelectLevel/SelectLevel.tsx";
 import ConfirmLevel from "./pages/ConfirmLevel/ConfirmLevel.tsx";
 import MakeBook from "./pages/Makebook/MakeBook.tsx";
@@ -25,8 +24,7 @@ const App = () => {
         <Route path="/CreateStory" element={<CreateStory />} />
         <Route path="/SelectChar" element={<SelectChar />} />
         <Route path="/ThemePageNext" element={<ThemePageNext />} />
-        <Route path="/BasicInfoAC" element={<BasicInfoAC />} />
-        <Route path="/BasicInfoDC" element={<BasicInfoDC />} />
+        <Route path="/BasicInfoPage" element={<BasicInfoPage />} />
         <Route path="/SelectLevel" element={<SelectLevel />} />
         <Route path="/ConfirmLevel" element={<ConfirmLevel />} />
         <Route path="/Makebook" element={<MakeBook />} />
