6caa47a908853c046f4cd5434ab61aac38103d82
yeni023 <yeeun3011@naver.com>
2024-04-13 01:47:35 +0900

    🔥

--- apps/genius/src/App.tsx
+++ apps/genius/src/App.tsx
@@ -21,7 +21,6 @@ import Tutorial from "./pages/Tutorial/Tutorial.tsx";
 import DalkongTutorial from "./pages/CharacterTutorial/DalkongTutorial.tsx";
 import AlkongTutorial from "./pages/CharacterTutorial/AlkongTutorial.tsx";
 import Navbar from "./pages/Navbar/Navbar.tsx";
-import SignUp from "./pages/SignUp/SignUp.tsx";
 import MyPage from "./pages/MyPage/MyPage.tsx";
 
 const App = () => {
@@ -48,10 +47,8 @@ const App = () => {
         <Route path="/Tutorial" element={<Tutorial />} />
         <Route path="/AlkongTutorial" element={<AlkongTutorial />} />
         <Route path="/DalkongTutorial" element={<DalkongTutorial />} />
-        <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Navbar" element={<Navbar />} />
         <Route path="/SignUp" element={<SignUp />} />
-
         <Route path="/MyPage" element={<MyPage />} />
       </Routes>
     </Router>
