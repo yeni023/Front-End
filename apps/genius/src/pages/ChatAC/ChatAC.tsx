baa8daa30410c0ea75c53d289f39afa720ef1e72
HYEJIN KIM <20_khj@naver.com>
2024-05-20 20:43:04 +0900

    선택형 동화 채팅 부 프리뷰 수정 완료

--- apps/genius/src/pages/ChatAC/ChatAC.tsx
+++ apps/genius/src/pages/ChatAC/ChatAC.tsx
@@ -12,9 +12,14 @@ import {
   finalChoices
 } from "./ACchatMessages";
 
+interface Message {
+  text: string;
+  isUser: boolean;
+}
+
 const ChatAC: React.FC = () => {
-  const [messages, setMessages] = useState(initialMessages("예은"));
-  const [selectedChoice, setSelectedChoice] = useState("");
+  const [messages, setMessages] = useState<Message[]>(initialMessages("예은"));
+  const [selectedChoice, setSelectedChoice] = useState<string>("");
   const [questionIndex, setQuestionIndex] = useState<number>(-1);
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const navigate = useNavigate();
@@ -39,7 +44,7 @@ const ChatAC: React.FC = () => {
     if (choice === initialChoices[1]) {
       setMessages([
         ...messages,
-        { text: initialChoices[1], isUser: true },
+        { text: choice, isUser: true },
         { ...notReadyMessage }
       ]);
     } else if (choice === initialChoices[0] || choice === finalChoices[1]) {
@@ -52,13 +57,19 @@ const ChatAC: React.FC = () => {
     } else if (choice === finalChoices[0]) {
       navigate("/MainHome");
     } else if (questionIndex !== -1) {
+      setMessages([...messages, { text: choice, isUser: true }]);
       const nextQuestionIndex = questionIndex + 1;
       if (nextQuestionIndex < questions.length) {
         setQuestionIndex(nextQuestionIndex);
-        setMessages([...messages, { text: ` ${choice}`, isUser: true }]);
+      } else {
+        // 마지막 선택지 후 처리
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { text: "이야기를 위한 선택지를 다 골랐어!", isUser: false }
+        ]);
+        setSelectedChoice("nextStep");
       }
     }
-    setSelectedChoice(choice);
   };
 
   return (
@@ -75,16 +86,18 @@ const ChatAC: React.FC = () => {
             </Styles.MessageContainer>
             {!message.isUser &&
               index === messages.length - 1 &&
-              (selectedChoice === "" ? (
+              (selectedChoice === "nextStep" ? (
                 <Choices
-                  choices={initialChoices}
-                  onSelect={handleChoiceSelect}
+                  choices={["다음으로"]}
+                  onSelect={() => navigate("/ACRoading")}
                 />
-              ) : selectedChoice === initialChoices[1] ? (
-                <Choices choices={finalChoices} onSelect={handleChoiceSelect} />
               ) : (
                 <Choices
-                  choices={questions[questionIndex].choices}
+                  choices={
+                    questionIndex >= 0 && questionIndex < questions.length
+                      ? questions[questionIndex].choices
+                      : initialChoices
+                  }
                   onSelect={handleChoiceSelect}
                 />
               ))}
